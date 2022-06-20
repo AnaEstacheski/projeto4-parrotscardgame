13bf056491c559c052913f03d239d93417d9b5bd
Ana Flávia Estacheski <flaviaestacheski@gmail.com>
2022-06-20 16:51:42 -0300

    layout e a script para sorteio de cartas aleatoriamente

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const cartas = prompt("Quantas cartas você quer no jogo?");
+while (cartas % 2 != 0 || cartas < 4 || cartas > 14) {
+    cartas = prompt("Insira um número par entre 4 e 14")
+}
+const pares = cartas / 2;
+const jogo = [];
+const tipos = [["./img/revertitparrot.gif"], ["./img/bobrossparrot.gif"], ["./img/explodyparrot.gif"], ["./img/fiestaparrot.gif"], ["./img/metalparrot.gif"], ["./img/tripletsparrot.gif"], ["./img/unicornparrot.gif"]];
+for (let i = 0; i < pares; i++) {
+    jogo.push(tipos[i]);
+    jogo.push(tipos[i]);
+}
+jogo.sort(() => {return Math.random() - 0.5});
